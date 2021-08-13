5c11d3ffac926277bc3bef3a9d087310d14b3f74
cepheus3 <nebiyouelias96@gmail.com>
2021-08-13 04:57:51 -0400

    first commit ANP

---
+++ index.php
@@ -0,0 +1,105 @@
+   <!-- database -->
+   <?php include "includes/db.php"; ?>
+
+
+   <!-- header html -->
+<?php include "includes/header.php"; ?>
+
+
+
+
+
+
+    <!-- Navigation -->
+    <?php include "includes/navigation.php"; ?>
+    
+
+
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                    <?php 
+
+                    $query = "SELECT * FROM post";
+
+                    $select = mysqli_query($connection,$query);
+
+                    while($row = mysqli_fetch_assoc($select)){
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_image = $row['post_image'];
+                        $post_content = $row['post_content'];
+                        ?>
+
+                   
+
+
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+
+
+
+
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"> <?php echo $post_title;?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author;?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date;?> </p>
+                <hr>
+                <img class="img-responsive" src="images/python-data.png" alt="">
+                <hr>
+                <p><?php echo $post_content;?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+
+                 <?php } ?>
+
+
+
+
+                <!-- Second Blog Post -->
+                
+
+
+
+
+                <!-- Third Blog Post -->
+              
+
+
+                <!-- Pager -->
+                <ul class="pager">
+                    <li class="previous">
+                        <a href="#">&larr; Older</a>
+                    </li>
+                    <li class="next">
+                        <a href="#">Newer &rarr;</a>
+                    </li>
+                </ul>
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+            <?php include "includes/side.php"; ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+   <?php include "includes/footer.php"; ?>
